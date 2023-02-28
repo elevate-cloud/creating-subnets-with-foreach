9b4d6026829e2dce4a9217d2c0c480a1ef491908
Levent <levent.eren@deop.ca>
2023-02-28 14:02:35 -0500

    var added

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "appgrp" {
-  name     = local.resource_group_name
+  name     = var.resource_group_name
   location = local.location  
 }
 
--- variables.tf
+++ variables.tf
@@ -3,3 +3,10 @@ variable "number_of_subnets" {
   description="This defines the number of subnets"
   default =2
 }
+
+variable "resource_group_name" {
+  type = string
+  value= app-grp
+
+  
+}
